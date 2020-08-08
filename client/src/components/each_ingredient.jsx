041faacb94e638bed17d37670d787d0abd0e4071
Your Name <you@example.com>
2020-08-08 14:58:10 -0700

    can edit

--- client/src/components/each_ingredient.jsx
+++ client/src/components/each_ingredient.jsx
@@ -8,21 +8,52 @@ export default class Ingredient extends React.Component {
         this.state = {
             openEditCapacity: false,
             openEditUsed: false,
-            openEditAvgNeed: false
+            openEditAvgNeed: false,
+            capacity: this.props.ingredient.capacity,
+            used: this.props.ingredient.used,
+            avgNeed: this.props.ingredient.average_need
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    toggleEdit(toToggle){
+        if (toToggle === "capacity"){
+            this.setState({openEditCapacity: !this.state.openEditCapacity})
+        }
+        else if (toToggle === "used"){
+            this.setState({openEditUsed: !this.state.openEditUsed})
+        }
+        else if (toToggle === "avg_need"){
+            this.setState({openEditAvgNeed: !this.state.openEditAvgNeed})
         }
     }
 
-    openEdit(){
-        this.setState({openEditCapacity: !this.state.openEditCapacity})
+    handleChange(e){
+        console.log(e.target.name, e.target.value)
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    updateChange(){
+        //change in backend
+    }
+
+    doneEdit(toClose){
+        this.toggleEdit(toClose)
+        this.updateChange()
     }
 
     render(){
         console.log('props in Ingredient', this.props)
+        console.log(this.state.capacity)
         const each = this.props.ingredient
         const {
             openEditCapacity,
             openEditAvgNeed,
-            openEditUsed
+            openEditUsed,
+            capacity,
+            used,
+            avgneed
         } = this.state
 
         return (
@@ -32,17 +63,58 @@ export default class Ingredient extends React.Component {
                         <td>{each.unit}</td>
                         <td>
                             {openEditCapacity 
-                            ? <input
-                                type="text"
-                              />
+                            ? <div>
+                                <input
+                                type="number"
+                                name="capacity"
+                                step="0.00"
+                                value={capacity}
+                                onChange={this.handleChange}
+                                />
+                                <button onClick={() => this.doneEdit("capacity")}>Ok</button>
+                              </div>
                             : <div>
-                                {each.capacity} 
-                                <span onClick={() => this.openEdit()}><FontAwesomeIcon icon={faEdit}/></span>
+                                {capacity} 
+                                <span onClick={() => this.toggleEdit("capacity")}><FontAwesomeIcon icon={faEdit}/></span>
                               </div>
                             }
                         </td>
-                        <td>{each.used} <span><FontAwesomeIcon icon={faEdit}/></span></td>
-                        <td>{each.average_need} <span><FontAwesomeIcon icon={faEdit}/></span></td>
+                        <td>
+                            {openEditUsed 
+                            ? <div>
+                                <input
+                                type="number"
+                                name="used"
+                                step="0.00"
+                                value={used}
+                                onChange={this.handleChange}
+                                />
+                                <button onClick={() => this.doneEdit("used")}>Ok</button>
+                              </div>
+                            : <div>
+                                {used} 
+                                <span onClick={() => this.toggleEdit("used")}><FontAwesomeIcon icon={faEdit}/></span>
+                              </div>
+                            }
+                        </td>
+                        <td>
+                            {openEditAvgNeed
+                            ? <div>
+                                <input
+                                type="number"
+                                step="0.00"
+                                value={avgneed}
+                                name="avgneed"
+                                onChange={this.handleChange}
+                                />
+                                <button onClick={() => this.doneEdit("avg_need")}>Ok</button>
+                              </div>
+                            : <div>
+                                {avgneed} 
+                                <span onClick={() => this.toggleEdit("avg_need")}><FontAwesomeIcon icon={faEdit}/></span>
+                            </div>
+                            }
+                        </td>
                         <td>{each.capacity - each.used}</td>
                         <td>{each.average_need-(each.capacity - each.used)}</td>
                         <td>{each.used}</td>
